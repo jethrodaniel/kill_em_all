4bbfeffe6cb7fddcf6c1aefe701cbc2be1585a54
Mark Delk <jethrodaniel@gmail.com>
2019-02-01 18:31:50 -0600

    fix a typo

--- scripts/kill_all_numeric_sessions.sh
+++ scripts/kill_all_numeric_sessions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo 'Killing all tmux sessions with numeric name (except the currently attached one) ...'
+echo 'Killing all tmux sessions with numeric names (except the currently attached one) ...'
 
 tmux ls | grep -v 'attached' \
         | cut -d':' -f 1     \
